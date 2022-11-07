f288ea8de1bb89e9f79e89f16454d3be502991d7
Deboissy <pdeboissy@deloitte.fr>
2022-11-07 15:08:36 +0100

    ordi deloitte

--- evaluation/jour6/projet.py
+++ evaluation/jour6/projet.py
@@ -5,9 +5,14 @@ from nbautoeval import CallRenderer, PPrintRenderer
 def identifiant(cour, date, numero):
     cour = cour.replace("paris", "Paris")
     cour = cour.replace("Pris", "Paris")
+    cour = cour.replace("\n", "")
 
     date = date.strip()
+    date = date.replace("\n", "")
 
+    numero = numero.replace("quinze", "15")
+    numero = numero.strip()
+    
     return cour + " " + date + " " + numero    
 # @END@
 
@@ -15,23 +20,23 @@ operands = [
     Args("CA Paris","   10 janvier 2018","n° 15/17379"),
     Args("CA Paris","12 janvier 2018","n° 15/20452*"),
     Args("CA Paris","12 janvier 2018","n° 15/22694"),
-    Args("CA Paris","17 janvier 2018","n° 15/17101"),
-    Args("CA paris","17 janvier 2018","n° 15/17249"),
+    Args("CA Paris","17 janvier 2018","n° 15/17101  "),
+    Args("CA paris\n","17 janvier 2018","n° quinze/17249"),
     Args("CA Paris","18 janvier 2018","n° 15/13129"),
-    Args("CA Pris","19 janvier 2018","n° 15/10939"),
-    Args("CA Paris","   19 janvier 2018","n° 17/11354"),
+    Args("CA Pris","19 janvier 2018\n","n° 15/10939 "),
+    Args("CA Paris","   19 janvier 2018","  n° 17/11354"),
     Args("CA Paris","19 janvier 2018","n° 15/23767"),
     Args("CA Paris","   25 janvier 2018","n° 15/16368"),
     Args("CA paris","1er février 2018","n° 15/13726"),
-    Args("CA Paris","7 février 2018	","n° 12/07788"),
-    Args("CA Pris","7 février 2018	","n° 15/05318"),
+    Args("CA Paris\n","7 février 2018	","n° 12/07788"),
+    Args("CA Pris","\n7 février 2018	","n° quinze/05318"),
     Args("CA Paris","7 février 2018	","n° 16/00080"),
     Args("CA paris","8 février 2018	","n° 15/12544"),
     Args("Cass. com.","14 février 2018","n° 16-24667"),
     Args("CA Paris","15 février 2018","n° 15/10066"),
-    Args("CA paris","15 février 2018","n° 15/13088"),
+    Args("CA paris","15 février 2018","n° 15/13088  "),
     Args("CA Pris","    21 février 2018","n° 15/05499"),
-    Args("CA Paris","21 février 2018","n° 15/06721"),
+    Args("CA Paris","21 février 2018","n° quinze/06721"),
     Args("CA Paris","21 février 2018","n° 15/08409"),
 ]
 
